c165171879b8c7632f442bc924d119dcb80cf8c6
JohnathanWells <slaifir@hotmail.com>
2020-01-22 09:58:59 -0500

    First Commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMov = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        this.transform.position = new Vector3(this.transform.position.x + xMov, this.transform.position.y, this.transform.position.z);
+    }
+}
